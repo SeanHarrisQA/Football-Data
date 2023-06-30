286cfc5ff64047cdd8c46278d0b19fa48c106f12
SeanHarrisQA <sharris3@qa.com>
2023-06-30 16:00:30 +0100

    amend file paths to load the json

--- pages/3_Player_Data.py
+++ pages/3_Player_Data.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 @st.cache_data
 def load_match_data():
-    with open('../open-data/data/events/3857261.json') as f:
+    with open('../../Python Learning/open-data/data/events/3857261.json') as f:
         wal_eng = json.load(f)
     we = pd.json_normalize(wal_eng, sep='_').assign(match_id="3857261")
     return we
